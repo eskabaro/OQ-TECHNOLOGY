a56a8dfa9c2ecd98c5459fc24ea423af4f5d4607
vmamchur <vadym.mamchur.job@gmail.com>
2024-05-23 22:57:20 +0300

    add mobile styles for in-the-media page

--- app/(pages)/in-the-media/page.tsx
+++ app/(pages)/in-the-media/page.tsx
@@ -3,14 +3,13 @@ import { FirstSection } from './_ui/first-section'
 import { Carousel } from '@/app/(app)/_components/carousel'
 import { images } from './_const/carousel-images'
 
+import styles from './_styles/InTheMedia.module.scss'
+
 export default function InTheMediaPage() {
     return (
-        <>
-            <Container as='section'>
-                <FirstSection />
-            </Container>
-
+        <div className={styles.wrapper}>
+            <FirstSection />
             <Carousel imagesSrc={images} />
-        </>
+        </div>
     )
 }
