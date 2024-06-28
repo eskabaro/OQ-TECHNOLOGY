b01da351183a3406c5779639b6768b0d4578f9fb
vmamchur <vadym.mamchur.job@gmail.com>
2024-06-28 17:35:14 +0300

    remove unused technology fourth section

--- app/(pages)/technology/_ui/third-section/index.tsx
+++ app/(pages)/technology/_ui/third-section/index.tsx
@@ -1,36 +1,38 @@
 'use client'
 
 import { FC } from 'react'
-import Image from 'next/image'
 
-import { Article } from '@/app/_shared/ui/typography/article'
+import useScrollSlider from '@/app/_shared/lib/hooks/useScrollSlider'
+import { videos } from '../../const/videos'
 import { SubTitle } from '@/app/_shared/ui/sub-title'
+import { VideoItem } from '../video-item'
+import { Slider } from '@/app/(app)/_components/slider'
 
-import styles from './Third.module.scss'
+import styles from './ThirdSection.module.scss'
 
 export const ThirdSection: FC = () => {
+    const { onEmblaApiInit, onSliderScroll } = useScrollSlider()
+
     return (
         <div className={styles.wrapper}>
-            <SubTitle upTitle='TECHNOLOGY' title='HIGH ALTITUDE PLATFORMS AND DRONES' />
-
-            <div className={styles.wrapper_content}>
-                <div className={styles['left-side']}>
-                    <Article
-                        titleTag='h2'
-                        title='Technology Description'
-                        paragraphs={[
-                            'OQ Technology developed the IP of cellular base station software and hardware that can function in power-limited non-terrestrial moving objects. Imagine the cell towers in the sky moving at more than 7 Km/s at distances of hundreds of kilometers away.',
-                            'This Base Station Box (the payload) can be fitted on LEO satellites, partner GEO hubs, or even HAPS and drones to provide instantaneous local or global 5G IoT coverage in poorly connected areas, or during disasters when terrestrial networks are no longer available.',
-                            'Because we build our own software we can control the quality and capacity of the service to give our users the best and most secure experience.'
-                        ]}
-                    />
-                </div>
-
-                <div className={styles['right-side']}>
-                    <Image src={require('../../_assets/ballon-1.webp')} width={630} height={280} alt='' />
-                    <Image src={require('../../_assets/ballon-2.webp')} width={630} height={280} alt='' />
-                </div>
-            </div>
+            <SubTitle
+                upTitle='TECHNOLOGY'
+                title='LEARN MORE FROM OUR PROFESSIONALS'
+                isButtonGroup
+                prevSlideFn={() => onSliderScroll('prev')}
+                nextSlideFn={() => onSliderScroll('next')}
+            />
+            <Slider
+                hasDots
+                onEmblaApiInit={onEmblaApiInit}
+                options={{
+                    align: 'start',
+                }}
+            >
+                {videos.map((video) => (
+                    <VideoItem key={video.id} {...video} />
+                ))}
+            </Slider>
         </div>
     )
 }
--- app/(pages)/technology/page.tsx
+++ app/(pages)/technology/page.tsx
@@ -2,15 +2,13 @@ import { Container } from '@/app/(app)/_components/container'
 import { FirstSection } from './_ui/first-section'
 import { SecondSection } from './_ui/second-section'
 import { ThirdSection } from './_ui/third-section'
-import { FourthSection } from './_ui/fourth-section'
 
 import styles from './_styles/Technology.module.scss'
 
 const components = {
     FirstSection,
     SecondSection,
-    ThirdSection,
-    FourthSection
+    ThirdSection
 }
 
 export default function TechnologyPage() {
