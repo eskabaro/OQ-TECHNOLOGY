92cdbf2540c66b87a7f28b078a2c804308cd9113
vmamchur <vadym.mamchur.job@gmail.com>
2024-08-01 19:49:20 +0300

    add progress animation to Metric component

--- app/(pages)/careers/_ui/second-section/_ui/metric/index.tsx
+++ app/(pages)/careers/_ui/second-section/_ui/metric/index.tsx
@@ -1,21 +1,24 @@
-import { FC } from 'react';
+'use client'
 
-import { IMetricItem } from '../../_const/metric-items';
+import { FC } from 'react'
 
-import styles from './Metric.module.scss';
+import { useIntersectionAnimation } from '@/app/_shared/lib/hooks/useIntersectionAnimation'
+import { IMetricItem } from '../../_const/metric-items'
 
-interface IMetricProps extends IMetricItem { }
+import styles from './Metric.module.scss'
+
+interface IMetricProps extends IMetricItem {}
 
 export const Metric: FC<IMetricProps> = ({ value, label }) => {
-  return (
-    <div className={styles.metric}>
-      <span className={styles.metric__value}>
-        {value}
-      </span>
-      <div className={styles.progress}>
-        <div className={styles.progress__circle} />
-      </div>
-      <p className={styles.metric__label}>{label}</p>
-    </div>
-  )
+    const { ref, isVisible } = useIntersectionAnimation()
+
+    return (
+        <div ref={ref} className={styles.metric}>
+            <span className={styles.metric__value}>{value}</span>
+            <div className={styles.progress}>
+                <div className={styles.progress__circle} style={{ left: isVisible ? value : '10px' }} />
+            </div>
+            <p className={styles.metric__label}>{label}</p>
+        </div>
+    )
 }
