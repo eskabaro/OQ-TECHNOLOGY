17e1ddaf6513ac94ea63bc9c2d31679d065b7981
vmamchur <vadym.mamchur.job@gmail.com>
2024-08-01 20:07:13 +0300

    fix Metric progress bar styles

--- app/(pages)/careers/_ui/fifth-section/index.tsx
+++ app/(pages)/careers/_ui/fifth-section/index.tsx
@@ -1,32 +1,36 @@
-"use client";
+'use client'
 
-import { useState } from 'react';
-import Image from 'next/image';
+import { useState } from 'react'
+import Image from 'next/image'
 
-import { cn } from '@/app/_shared/lib/classnames';
-import { Container } from '@/app/(app)/_components/container';
-import { steps } from './_const/steps';
+import { cn } from '@/app/_shared/lib/classnames'
+import { Container } from '@/app/(app)/_components/container'
+import { steps } from './_const/steps'
 
-import styles from './FifthSection.module.scss';
+import styles from './FifthSection.module.scss'
 
 export const FifthSection = () => {
-  const [selectedStepIndex, setSelectedStepIndex] = useState(0);
+    const [selectedStepIndex, setSelectedStepIndex] = useState(0)
 
-  return (
-    <Container className={styles.wrapper} as='section'>
-      <div className={styles.stepper}>
-        {steps.map((_, index) => (
-          <div key={index} className={cn(styles.stepper__item, index === selectedStepIndex && styles.active)} onClick={() => setSelectedStepIndex(index)}>
-            <span className={styles.stepper__item__number}>Step {index + 1}</span>
-          </div>
-        ))}
-      </div>
+    return (
+        <Container className={styles.wrapper} as='section'>
+            <div className={styles.stepper}>
+                {steps.map((_, index) => (
+                    <button
+                        key={index}
+                        className={cn(styles.stepper__item, index === selectedStepIndex && styles.active)}
+                        onClick={() => setSelectedStepIndex(index)}
+                    >
+                        <span className={styles.stepper__item__number}>Step {index + 1}</span>
+                    </button>
+                ))}
+            </div>
 
-      <div className={styles.content}>
-        <h3 className={styles.title}>{steps[selectedStepIndex].title}</h3>
-        <p className={styles.description}>{steps[selectedStepIndex].description}</p>
-        <Image src={steps[selectedStepIndex].imageSrc} alt='' />
-      </div>
-    </Container>
-  );
+            <div className={styles.content}>
+                <h3 className={styles.title}>{steps[selectedStepIndex].title}</h3>
+                <p className={styles.description}>{steps[selectedStepIndex].description}</p>
+                <Image src={steps[selectedStepIndex].imageSrc} alt='' />
+            </div>
+        </Container>
+    )
 }
